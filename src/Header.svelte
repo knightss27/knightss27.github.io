fd04be8b850c8ef01571512650ce33a4f6047745
Seth Knights <knightss@catlin.edu>
2021-04-05 22:46:42 -0700

    New design, just trying things out.

---
+++ src/Header.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<div>
+  <img class="s" src="/svg/S.svg" alt="S Character" />
+  <img src="/svg/E.svg" alt="E Character" />
+  <img src="/svg/T.svg" alt="T Character" />
+  <img src="/svg/H.svg" alt="H Character" />
+</div>
+
+<style>
+  div {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    margin-bottom: 2rem;
+    flex-wrap: wrap;
+    margin-top: 4rem;
+  }
+
+  @media only screen and (min-width: 800px) {
+    div {
+      margin-top: 8rem;
+    }
+  }
+
+  img {
+    margin: 0rem 0.5rem;
+    height: 125px;
+  }
+
+  img.s {
+    margin: 0rem 0rem;
+  }
+</style>
