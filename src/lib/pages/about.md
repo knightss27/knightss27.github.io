e195afed57e738a9256e3174a48005e5a31c15d4
knightss27 <knightss27@protonmail.com>
2023-07-29 00:24:25 -0700

    feat: clean up + optimize

--- src/lib/pages/about.md
+++ src/lib/pages/about.md
@@ -5,4 +5,4 @@ I'm interested in many things - but mostly rockets and robots. I am an active me
 As it stands, I've written in many programming languages - most significantly: C, Javascript, Python and Java. I started programming about 5 years ago and have (for the most part) loved it since.
 Outside of writing code, I play bass and previously captained both the Mock Trial and Speech & Debate teams at my high school.
 <br><br>
-View my Github [here](https://github.com/knightss27).
+View [my Github](https://github.com/knightss27).
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,15 @@
 <script>
-    import profile from "$lib/assets/headshot.png";
+    import profile from "$lib/assets/headshot.webp";
     import SvelteMarkdown from 'svelte-markdown';
     import about from "$lib/pages/about.md?raw";
 </script>
 
 <div>
-    <img alt="headshot" src={profile} />
+    <picture>
+        <source media="(max-width: 799px)" srcset={profile} width={150} height={150}/>
+        <source media="(min-width: 800px)" srcset={profile} width={300} height={300} />
+        <img src={profile} alt="Puppy with balloons" />
+      </picture>
     <h2>about me</h2>
     <p><SvelteMarkdown source={about} isInline /></p>
 </div>
@@ -18,8 +22,6 @@
         max-width: 800px;
     }
     img {
-        width: 300px;
-        max-width: 50%;
         border-radius: 50%;
     }
     h2 {
